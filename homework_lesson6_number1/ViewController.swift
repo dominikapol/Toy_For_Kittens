0bb61a2cc04d766acf834bf8ac1bc5f729aa29a9
Dominika <dominikapiganova@mail.ru>
2021-08-06 18:38:43 +0300

    Initial Commit

---
+++ homework_lesson6_number1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework_lesson6_number1
+//
+//  Created by Dominika Poleshyck on 6.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
