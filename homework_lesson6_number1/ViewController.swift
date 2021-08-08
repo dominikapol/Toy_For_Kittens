f7fdb567e9959af9483ee11144845d89f59b49c7
Dominika <dominikapiganova@mail.ru>
2021-08-08 15:31:23 +0300

    This is toy for kittens :з

--- homework_lesson6_number1/ViewController.swift
+++ homework_lesson6_number1/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+class ViewController: UIViewController {
+    var emptyView: UIView = UIView()
+    var button: UIButton = UIButton(frame: CGRect(x: 150, y: 250, width: 50, height: 50))
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        button.backgroundColor = .orange
+        button.setTitle("meow", for: .normal)
+        button.layer.cornerRadius = 25
+        button.addTarget(self, action: #selector(pressButton), for: .touchUpInside)
+        view.addSubview(button)
+        button.tag = 1
+        self.view.addSubview(button)
+    }
+    
+    @objc func pressButton(sender: UIButton) {
+        let btnsendtag: UIButton = sender
+        if btnsendtag.tag == 1 {
+
+        dismiss(animated: true, completion: nil)
+            button.frame = CGRect(x: Int.random(in: 10...300), y: Int.random(in: 10...700), width: 50, height: 50)
+    }
     }
 
 
 }
-
